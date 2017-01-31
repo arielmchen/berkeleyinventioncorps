497899053ff18dd7f17798f3fec3fde1df153f80
Anish Saha <asaha@berkeley.edu>
2017-01-31 12:33:43 -0800

    Update sendmail.php

--- php/sendmail.php
+++ php/sendmail.php
@@ -15,10 +15,8 @@ $headers = 'From: ' . $_POST["name"] . '<' . $_POST["email"] . '>' . "\r\n" .
     'Reply-To: ' . $_POST["email"] . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
 
-  //
-  mail( "vijayanpp02@gmail.com", $_POST['message'], $headers );
+  mail( "berkeleyinventioncorps@gmail.com", $_POST['message'], $headers );
  
-  //      ^
   //  Replace with your email 
 }
 ?>
